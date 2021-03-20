a86dd2270f2d1691a56f0bf9777002dc90569ba4
Grant Spencer <grantspencer95@gmail.com>
2021-03-20 14:12:16 -0400

    minor updates
    testing

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 'use strict'
+
+// const fetch = require('node-fetch');
+
+async function getThumbnail() {
+    let responsejson = await fetch('https://openlibrary.org/api/books?bibkeys=ISBN:9780980200447&jscmd=data&format=json');
+    let response = await responsejson.json();
+
+    let isbn = Object.keys(response)[0];
+
+    let cover = response[isbn].cover.medium;
+
+    cover = String(cover);
+
+    let image = document.getElementById('book-thumbnail');
+    image.src = cover;
+
+    return cover;
+
+}
+
+getThumbnail();
