ccc819e33f3172b34d400d52ffbd87820548bdcf
Grant Spencer <grantspencer95@gmail.com>
2021-03-27 15:12:37 -0400

    Added content, scrollable divs etc.

--- script.js
+++ script.js
@@ -12,11 +12,29 @@ async function getThumbnail() {
 
     cover = String(cover);
 
-    let image = document.getElementById('book-thumbnail');
-    image.src = cover;
+    let image = document.getElementsByClassName('book-thumbnail');
+    image[0].src = cover;
 
     return cover;
 
 }
 
 getThumbnail();
+
+async function getBookCover() {
+    let responsejson = await fetch('https://openlibrary.org/api/books?bibkeys=ISBN:9780980200447&jscmd=data&format=json');
+    let response = await responsejson.json();
+
+    let isbn = Object.keys(response)[0];
+
+    let cover = response[isbn].cover.large;
+
+    cover = String(cover);
+
+    let image = document.getElementById('book-cover');
+    image.src = cover;
+
+    return cover;
+}
+
+getBookCover();
